484d6daa1780847c392810cf846d269b1290d90c
unknown <vishnuprasadw@gmail.com>
2024-04-21 15:28:49 +0530

    intial files

---
+++ src/imageUploader.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import TextRecognition from './TextRecognition';
+const ImageUploader = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const handleImageUpload = (event) => {
+    const image = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(image));
+  };
+  return (
+    <>
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {selectedImage && <img src={selectedImage} alt="Selected" />}
+    </div>
+    <><TextRecognition selectedImage={selectedImage}/></></>
+  );
+};
+export default ImageUploader;
