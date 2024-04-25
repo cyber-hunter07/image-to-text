539952c934a8b5e01897edacabeb1e286d5229f0
unknown <vishnuprasadw@gmail.com>
2024-04-25 08:07:03 +0530

    valid document corections

--- src/TextRecognition.js
+++ src/TextRecognition.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 import Tesseract from "tesseract.js";
+
 const TextRecognition = ({ selectedImage }) => {
   const [recognizedText, setRecognizedText] = useState("");
   useEffect(() => {
@@ -18,12 +19,29 @@ const TextRecognition = ({ selectedImage }) => {
     });
     saveAs(file, "recognizedText.txt");
   };
+  let returnValue;
+  if (recognizedText.includes("DRIVING LICENCE")) {
+    returnValue = "Valid Document";
+  } else if (recognizedText === "") {
+    returnValue = "";
+  } else {
+    returnValue = "Invalid Document";
+  }
+
+  console.log(
+    recognizedText.split(" "),
+    recognizedText.includes("DRIVING LICENCE")
+  );
+
   return (
     <div>
       <h5>Recognized Text:</h5>
       <p>{recognizedText}</p>
       <>
         <button onClick={handleDownload}>Download</button>
+      </>
+      <>
+        <h5>Valid Document: {returnValue}</h5>
       </>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ImageUploader from './imageUploader'
-import TextRecognition from './TextRecognition';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
