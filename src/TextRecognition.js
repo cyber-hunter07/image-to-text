6be0fa873c07671972c536fe39b83312140ce017
unknown <vishnuprasadw@gmail.com>
2024-10-19 19:37:38 +0530

    new validation

--- src/TextRecognition.js
+++ src/TextRecognition.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { saveAs } from "file-saver";
 import Tesseract from "tesseract.js";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 const TextRecognition = ({ selectedImage }) => {
   const [recognizedText, setRecognizedText] = useState("");
@@ -13,20 +14,42 @@ const TextRecognition = ({ selectedImage }) => {
     };
     recognizeText();
   }, [selectedImage]);
-  const handleDownload = () => {
-    window.location.href = 'http://myprojectshub.co.in/driving/upload.php';
-    const file = new Blob([recognizedText], {
-      type: "text/plain;charset=utf-8",
-    });
-    saveAs(file, "recognizedText.txt");
+  // const handleDownload = () => {
+  //   window.location.href = 'http://myprojectshub.co.in/driving/upload.php';
+  //   const file = new Blob([recognizedText], {
+  //     type: "text/plain;charset=utf-8",
+  //   });
+  //   saveAs(file, "recognizedText.txt");
+  // };
+
+  const { speak,cancel } = useSpeechSynthesis();
+  const handleStop = () => {
+    cancel(); // This will stop the speech
   };
   return (
     <div>
       <h5>Recognized Text:</h5>
-      <p style={{ color: "whitesmoke" }}>{recognizedText}</p>
-      <>
+      {/* <p style={{ color: "whitesmoke" }}>{recognizedText}</p> */}
+      <textarea
+        value={recognizedText}
+        onChange={(event) => setRecognizedText(event.target.value)}
+        rows="5"
+        cols="50"
+      />
+      {/* <div>
         <button onClick={handleDownload}>Verify</button>
-      </>
+
+      </div> */}
+     <> <div className="group">
+                <button onClick={() => speak({ text:recognizedText  })}>
+                    Speech
+                </button>
+            </div></>
+            <div className="group">
+                <button onClick={handleStop}>
+                    STOP
+                </button>
+            </div>
     </div>
   );
 };
